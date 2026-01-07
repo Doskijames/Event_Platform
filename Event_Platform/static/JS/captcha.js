a143ac762087770610d3b9ed2ab22338608186d2
Doskijames <dehindeaba@gmail.com>
2026-01-07 12:12:15 +0000

    Delete Event_Platform directory

--- Event_Platform/static/JS/captcha.js
+++
@@ -1,8 +0,0 @@
-document.addEventListener("DOMContentLoaded", () => {
-  const captcha = document.getElementById("captcha");
-  if (!captcha) return;
-
-  captcha.addEventListener("input", () => {
-    captcha.value = captcha.value.replace(/[^0-9]/g, "");
-  });
-});
